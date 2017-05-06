509b41773668bd9c2dde19972336222df424e9c0
Tom Elam <tomelam@gmail.com>
2017-05-06 19:57:21 +0530

    Added .unicorn.sh for production systems. Added empty directories shared/log, shared/pids, shared/sockets for nginx. Committing precompiled assets.

---
+++ .unicorn.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This shell script gets called about every 10 seconds when unicorn is down.
+
+set -e
+
+# app settings
+USER="rails"
+APP_NAME="mashwebclub"
+APP_ROOT="/home/$USER/$APP_NAME"
+#ENV="development"
+ENV="development"
+
+# environment settings
+PATH="/home/$USER/.rbenv/shims:/home/$USER/.rbenv/bin:$PATH"
+# CMD="cd $APP_ROOT && bundle exec unicorn -c config/unicorn.rb -E $ENV -D" >>/home/rails/mashwebclub.unicorn.sh.out
+PID="$APP_ROOT/shared/pids/unicorn.pid"
+OLD_PID="$PID.oldbin"
+
+RAILS_ENV="development" PATH="/home/$USER/.rbenv/shims:/home/$USER/.rbenv/bin:$PATH" bundle exec unicorn -c config/unicorn.rb -E $ENV -D
