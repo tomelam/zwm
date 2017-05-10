ef6685b3a8d6b848ec55ccad1c0a7e8a932acd6f
Tom Elam <tomelam@gmail.com>
2017-05-10 14:54:01 +0530

    Removed .rbenv-vars from repo and changed all passwords and keys. Simplified .unicorn.sh and set it for production mode.

--- .unicorn.sh
+++ .unicorn.sh
@@ -6,15 +6,8 @@ set -e
 
 # app settings
 USER="rails"
-APP_NAME="zwm"
-APP_ROOT="/home/$USER/$APP_NAME"
-#ENV="development"
-ENV="development"
 
 # environment settings
 PATH="/home/$USER/.rbenv/shims:/home/$USER/.rbenv/bin:$PATH"
-# CMD="cd $APP_ROOT && bundle exec unicorn -c config/unicorn.rb -E $ENV -D" >>/home/rails/mashwebclub.unicorn.sh.out
-PID="$APP_ROOT/shared/pids/unicorn.pid"
-OLD_PID="$PID.oldbin"
 
-RAILS_ENV="development" PATH="/home/$USER/.rbenv/shims:/home/$USER/.rbenv/bin:$PATH" bundle exec unicorn -c config/unicorn.rb -E $ENV -D
+RAILS_ENV="production" PATH="/home/$USER/.rbenv/shims:/home/$USER/.rbenv/bin:$PATH" bundle exec unicorn -c config/unicorn.rb -E $ENV -D
